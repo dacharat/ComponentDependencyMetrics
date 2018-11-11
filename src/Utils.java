9f24d1b98ee9ea8260c3b9a04957a0a47a54f278
dacharat <dacharat2013@gmail.com>
2018-11-11 14:39:35 +0700

    :fireworks: first commit

---
+++ src/Utils.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.text.DecimalFormat;
+
+public class Utils {
+
+	public static void main(String[] args) {
+
+		File jcdFile = new File("/Users/jack/eclipse-workspace/groovy/src/main/java/org/codehaus/groovy");
+
+		String[] jcdFiles = jcdFile.list();
+		FileRead read = new FileRead();
+
+		System.out.println("Files Found: " + jcdFiles.length);
+
+		for (String myFile : jcdFiles) {
+			if (myFile.contains(".java"))
+				read.readInFile(jcdFile.toString(), myFile);
+		}
+
+		PackageInfo pack = new PackageInfo(read.getNa(), read.getNc(), read.getCa(), read.getCe());
+		DecimalFormat df = new DecimalFormat("#.######");
+
+		System.out.println("ca: " + read.getCa());
+		System.out.println("ce: " + read.getCe());
+		System.out.println("nc: " + read.getNc());
+		System.out.println("na: " + read.getNa());
+		System.out.println("Instability: " + df.format(pack.getInstability()));
+		System.out.println("Abstractness: " + df.format(pack.getAbstractness()));
+
+	}
+
+}
